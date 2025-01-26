3189afebde0a19fd554969987a02785d0e034c18
Dmitriy Trifonov <trifonov.dmitriy.m@gmail.com>
2025-01-26 22:33:29 +0400

    Extract data tests to separate test-suite

--- Assets/Scripts/Tests/Editor/Mirror/MatrixTests.cs
+++ Assets/Scripts/Tests/Editor/Mirror/MatrixTests.cs
@@ -9,28 +9,6 @@ namespace Tests.Editor.Mirror
     {
         [Test]
         [TestCaseSource(typeof(TestsData), nameof(TestsData.MatrixTestCaseSource))]
-        public void DataSceneTest(Data d)
-        {
-            new DataDisplayScene(d, TestContext.CurrentContext.Test.Name).Render();
-        }
-
-        [Test]
-        [TestCaseSource(typeof(TestsData), nameof(TestsData.MatrixTestCaseSource))]
-        public void DataTextureTest(Data d)
-        {
-            new DataDisplayTexture(d, TestContext.CurrentContext.Test.Name).Render();
-        }
-
-        [Test]
-        [TestCaseSource(typeof(TestsData), nameof(TestsData.MatrixTestCaseSource))]
-        public void DataTextTest(Data d)
-        {
-            var result = new DataDisplayText(d).Render();
-            Debug.Log(result);
-        }
-
-        [Test]
-        [TestCaseSource(typeof(TestsData), nameof(TestsData.MatrixTestCaseSource))]
         public void MatrixPositionTest(Data d)
         {
             var aGo = new GameObject("A");
---
+++ Assets/Scripts/Tests/Editor/Portal/DataTests.cs
@@ -0,0 +1,31 @@
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Tests.Editor.Portal
+{
+    [TestFixture]
+    public class DataTests
+    {
+        [Test]
+        [TestCaseSource(typeof(TestsData), nameof(TestsData.MatrixTestCaseSource))]
+        public void DataSceneTest(Data d)
+        {
+            new DataDisplayScene(d, TestContext.CurrentContext.Test.Name).Render();
+        }
+
+        [Test]
+        [TestCaseSource(typeof(TestsData), nameof(TestsData.MatrixTestCaseSource))]
+        public void DataTextureTest(Data d)
+        {
+            new DataDisplayTexture(d, TestContext.CurrentContext.Test.Name).Render();
+        }
+
+        [Test]
+        [TestCaseSource(typeof(TestsData), nameof(TestsData.MatrixTestCaseSource))]
+        public void DataTextTest(Data d)
+        {
+            var result = new DataDisplayText(d).Render();
+            Debug.Log(result);
+        }
+    }
+}
